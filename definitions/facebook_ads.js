12c30850a7c9c988991682b1fdf0cd2daf62bb54
Matt Strautmann <matt.strautmann@gmail.com>
2021-03-09 15:49:06 -0800

    Update facebook_ads.js

--- definitions/facebook_ads.js
+++ definitions/facebook_ads.js
@@ -3,8 +3,8 @@ const facebook = require("../");
 const facebookModels = facebook({
     //
     // Define the database and schema of your facebook data
-    facebookDatabase: "raw",
-    facebookSchema: "facebook_ads",
+    databaseName: "raw",
+    schemaName: "facebook_ads",
     // Optional prefix metadata if applicable
     stagingTablePrefix: "stg_",
     // Define the destination schema and table/view
